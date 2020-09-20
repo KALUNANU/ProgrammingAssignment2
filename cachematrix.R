a2f03878648894d93a7bc6646df746c1e33d9b3b
KALUNANU <44564024+KALUNANU@users.noreply.github.com>
2020-09-20 09:52:25 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,32 @@
 
-# makeCacheMatrix makes a matrix
-#cacheSolve is used to cache null matrix
-
-
-cacheSolve<- function(x=matrix()) {
-        
-        if(is.null(m)) {
-                message("getting cached data")
-              
-        }
-        
-        m
+makeCacheMatrix<-function(x=matrix()){
+     #creating null special matrix object with null inverse As A default
+     inverse<-NULL
+     set<-function(y){
+          #assigning new matrix and cleaning previous cache inverse
+          x<<-y
+          inverse<<-NULL
+     }
+     #functions to get,set inverse and original input matrix
+     get<-function() x
+     setInverse<- function(inverse) i<<-inverse #caching new inverse value, which will be provided by cacheSolve
+     getInverse<-function() i
+     #returning special matrix in list object
+     list(set = set, get = get,
+          setInverse = setInverse,
+          getInverse = getInverse)
 }
-makeCacheMatrix<-function(x = matrix()) {
-        m<-solve(x)
-        solve(m)
-}
+
+## Write a short comment describing this function
+
+cacheSolve<-function(matrix, ...){ #constructing function and ... given for further arguments for solve()
+     inverse<-matrix$getInverse()#getting cached inverse
+     if(!is.null(inverse)){#if inverse previously calculated send message to user and return cached
+          message("getting cached data")
+          return(inverse)
+     }
+     data<-matrix$get()#get matrix data in list
+     inverse<-solve(matrix, ...)#calculating inverse matrix assuming it is invertable
+     matrix$setInverse(inverse)#caching calculated inverse to our "special" matrix which is a list
+     inverse#returning inverse
+} 
